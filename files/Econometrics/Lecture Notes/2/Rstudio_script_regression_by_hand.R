5f1b7a3d4ab4864f09a146ff6e1e4c717ab967f4
henriqueveras <henriqueverasdepaiva@gmail.com>
2025-11-29 11:45:15 -0300

    Still making updates on the Econometrics files

--- files/Econometrics/Lecture Notes/2/Rstudio_script_regression_by_hand.R
+++ files/Econometrics/Lecture Notes/2/Rstudio_script_regression_by_hand.R
@@ -1,5 +1,7 @@
 #Estimating the OLS model NA TORA
 
+rm(list = ls())            # Remove todos os objetos do ambiente
+
 y <- matrix(c(6,11,4,3),4,1)
 y
 
@@ -32,6 +34,11 @@ y.hat
 e <- y - y.hat
 e
 
+#Get sum of fitted and resid
+y.model <- y.hat + e
+y.model
+y
+
 #Getting the M and P matrices
 X.X.prime.X.inv.X.prime<-X %*% X.prime.X.inv.X.prime
 X.X.prime.X.inv.X.prime
@@ -43,10 +50,13 @@ P<- X.X.prime.X.inv.X.prime
 
 #Compare residuals with the ones obtained before
 M %*% y
+e
 P %*% y
+y.hat
 
 #Create partition with X1=(1 1 1 1):
 X1 <- matrix(c(1,1,1,1),4,1)
+X1
 
 X1.prime <- t(X1)
 X1.prime
@@ -69,7 +79,17 @@ identity<-diag(4)
 M1<-identity - X1.X1.prime.X1.inv.X1.prime
 P1<- X1.X1.prime.X1.inv.X1.prime
 
-y_dev <- M1 %*% y
+#Getting variables as deviations from mean
+y_deviations <- M1 %*% y
+y_deviations
+X2 <- matrix(c(4,7,2,1,2,6,9,4),4,2)
+X2
+X2_deviations <- M1 %*% X2
+
+#Estimating the model using variables in deviation format
+lm(y_deviations ~ X2_deviations)
+
+lm(y~0+X)
 
 #Find squared sum of total
 y.prime<-t(y)
@@ -79,3 +99,11 @@ sst <- y.prime %*% M1 %*% y
 e.prime<-t(e)
 e.prime.e <- e.prime %*% e
 e.prime.e
+
+#Find R-squared
+R2 <- 1- e.prime.e/sst
+R2
+
+#Find adjusted R-squared
+adj_R2 <- 1 - ((nrow(X)-1)/(nrow(X)-ncol(X)))*(1-R2)
+adj_R2
